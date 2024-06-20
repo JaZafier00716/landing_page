5a13e4e0bbb20f3741e42732b3d5ee618a2a6e5d
JaZafier <jan.zamostny04@gmail.com>
2024-06-20 15:40:09 +0200

    modified:   app/layout.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const jetbrainsMono = JetBrains_Mono({ 
   subsets: ["latin"], 
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={`h-full ${jetbrainsMono.variable}`}>
         <Header />
         {children}
+        <SpeedInsights/>
         </body>
     </html>
   );
